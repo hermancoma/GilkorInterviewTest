ac19f9ad2b6f2557fa810c82b7bb4c2c6ab51360
Herman Sophian Putra <herman_sophian@yahoo.com>
2013-01-14 20:01:04 +0700

    First git commit
    Number Series : unsolved
    DoorProblem : solved

---
+++ DoorProblemResult.php
@@ -0,0 +1,51 @@
+<center> 
+<h1><a href="index.html"> Gilkor Interview Test </a></h1>
+
+<a href="DoorProblem.html"><p> <b> Door Problem </b> <br /></a></p>
+
+
+<?php
+$maxDoor = $_POST["maxDoor"];
+if ($maxDoor != 0)
+{
+	//$maxDoor = 10;
+	$maxEmployee = 10;
+	$door[]=array($maxDoor);
+
+	for ($i=1; $i<=$maxDoor; $i++)
+		{
+			$door[$i] = 0;
+		}
+		
+		for ($employeeCounter=1; $employeeCounter <= $maxEmployee ; $employeeCounter++ )
+		{
+			for ($doorCounter=1; $doorCounter <= $maxDoor ; $doorCounter++)
+			{
+				if (($doorCounter >= $employeeCounter)&&(($doorCounter%$employeeCounter)==0 ))
+				{
+					if ($door[$doorCounter] == 0)
+					{
+						$door[$doorCounter] = 1;
+					}
+					else
+					{
+						$door[$doorCounter] = 0;
+					}
+				}
+			}
+		}
+		for ($doorCounter=1; $doorCounter <= $maxDoor ; $doorCounter++)
+  		{
+			if ($door[$doorCounter] == 1)
+			{
+		  		echo "The door number " . $doorCounter . " is Opened <br>";
+			}
+			else
+			{
+				echo "The door number " . $doorCounter . " is Closed <br>";
+			}
+		}
+}
+?>
+
+</center>
---
+++ NumberSeries.php
@@ -0,0 +1,13 @@
+<center> 
+<h1><a href="index.html"> Gilkor Interview Test </a></h1>
+
+<p> <b> Number Series </b> <br /> </p>
+
+There is a series of number which is : <br/>
+2, 8, 12, 18, 20, 21, ..., ... <br/><br/><br/>
+
+The next number of the series would be : <br/>
+ ..., ...
+
+</center>
+ 
